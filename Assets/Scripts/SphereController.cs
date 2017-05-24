367c5c435e56eea93e1ff9646df66516c2a40ff0
Maryadi <maryadi@astrnt.co>
2017-05-24 14:57:53 +0700

    Script for Part II - Detecting keyboard input

--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class SphereController : MonoBehaviour {
 
+	private Rigidbody rb;
+	public float speed;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void FixedUpdate () {
+		float horizontalValue = Input.GetAxis ("Horizontal");
+		float verticalValue = Input.GetAxis ("Vertical");
+
+		Vector3 v3 = new Vector3 (horizontalValue, 0.0f, verticalValue);
+		rb.AddForce (v3 * speed * Time.deltaTime);
 	}
 }
