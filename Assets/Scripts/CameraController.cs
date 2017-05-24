8f83032152151c3ca2da2ce7108767022fe36b51
Maryadi <maryadi@astrnt.co>
2017-05-24 16:16:33 +0700

    Part 3 - CameraController Script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject sphere;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Last non state is best for LateUpdate
+	void LateUpdate () {
+		transform.position = sphere.transform.position + offset;
+	}
+}
